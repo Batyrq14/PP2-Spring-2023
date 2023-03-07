5c8ffee9a283338c1b5ae0480df5db50d015b52c
Batyrq14 <122871602+Batyrq14@users.noreply.github.com>
2023-03-07 17:15:49 +0600

    Update date.py

--- TSIS_4/date.py
+++ TSIS_4/date.py
@@ -27,7 +27,7 @@ def date_diff_in_Seconds(dt2, dt1):
   timedelta = dt2 - dt1
   return timedelta.days * 24 * 3600 + timedelta.seconds
 #Specified date
-date1 = datetime.strptime('2015-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
+date1 = datetime.strptime('2022-02-24 23:50:01', '%Y-%m-%d %H:%M:%S')
 #Current date
 date2 = datetime.now()
 print("\n%d seconds" %(date_diff_in_Seconds(date2, date1)))
